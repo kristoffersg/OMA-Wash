7bf465bde4911f245e6c56d9a4c0828a644d534f
lassebue <lassebuesvendsen@gmail.com>
2015-05-30 17:52:39 +0200

    ViewPagerAdapter

---
+++ GitRepo/app/src/main/java/com/example/ksg/omawash/DayFragmentAdapter.java
@@ -0,0 +1,33 @@
+package com.example.ksg.omawash;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v13.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lassebuesvendsen on 30/05/15.
+ */
+public class DayFragmentAdapter extends FragmentPagerAdapter
+{
+    ArrayList<ArrayList<ISlotItem>> weekList;
+    ArrayList<ISlotItem>            dayList;
+
+    public DayFragmentAdapter(ArrayList<ArrayList<ISlotItem>> weekList,FragmentManager manager)
+    {
+        super(manager);
+        this.weekList = weekList;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        dayList = weekList.get(position);
+        return DayFragment.newInstance(dayList);
+    }
+
+    @Override
+    public int getCount() {
+        return 7;
+    }
+}
